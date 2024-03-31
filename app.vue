21ab29fd3762e988bfa47e2c2db928c819dc0c4f
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2024-03-30 22:50:46 -0300

    feat: add daisyUI

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div data-theme="pastel">
+    <main class="h-screen w-full">
+      <h1>Hello world</h1>
+      <button class="btn btn-warning">Test button</button>
+    </main>
   </div>
 </template>
